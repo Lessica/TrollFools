8b268835da393a9c0ad42bd5042949d86ec17672
82Flex <82flex@gmail.com>
2024-07-21 22:02:38 +0800

    feat: use aux exec
    Signed-off-by: 82Flex <82flex@gmail.com>

---
+++ TrollFools/AuxiliaryExecute.swift
@@ -0,0 +1,155 @@
+//
+//  AuxiliaryExecute.swift
+//  MyYearWithGit
+//
+//  Created by Lakr Aream on 2021/11/27.
+//
+
+import Foundation
+
+/// Execute command or shell with posix, shared with AuxiliaryExecute.local
+public class AuxiliaryExecute {
+    /// we do not recommend you to subclass this singleton
+    public static let local = AuxiliaryExecute()
+
+    // if binary not found when you call the shell api
+    // we will take some time to rebuild the bianry table each time
+    // -->>> this is a time-heavy-task
+    // so use binaryLocationFor(command:) to cache it if needed
+
+    // system path
+    internal var currentPath: [String] = []
+    // system binary table
+    internal var binaryTable: [String: String] = [:]
+
+    // for you to put your own search path
+    internal var extraSearchPath: [String] = []
+    // for you to set your own binary table and will be used firstly
+    // if you set nil here
+    // -> we will return nil even the binary found in system path
+    internal var overwriteTable: [String: String?] = [:]
+
+    // this value is used when providing 0 or negative timeout paramete
+    internal static let maxTimeoutValue: Double = 2_147_483_647
+
+    /// when reading from file pipe, must called from async queue
+    internal static let pipeControlQueue = DispatchQueue(
+        label: "wiki.qaq.AuxiliaryExecute.pipeRead",
+        attributes: .concurrent
+    )
+
+    /// when killing process or monitoring events from process, must called from async queue
+    /// we are making this queue serial queue so won't called at the same time when timeout
+    internal static let processControlQueue = DispatchQueue(
+        label: "wiki.qaq.AuxiliaryExecute.processControl",
+        attributes: []
+    )
+
+    /// used for setting binary table, avoid crash
+    internal let lock = NSLock()
+
+    /// nope!
+    private init() {
+        // no need to setup binary table
+        // we will make call to it when you call the shell api
+        // if you only use the spawn api
+        // we don't need to setup the hole table cause it‘s time-heavy-task
+    }
+
+    /// Execution Error, do the localization your self
+    public enum ExecuteError: Error, LocalizedError, Codable {
+        // not found in path
+        case commandNotFound
+        // invalid, may be missing, wrong permission or any other reason
+        case commandInvalid
+        // fcntl failed
+        case openFilePipeFailed
+        // posix failed
+        case posixSpawnFailed
+        // waitpid failed
+        case waitPidFailed
+        // timeout when execute
+        case timeout
+    }
+
+    public enum TerminationReason: Codable {
+        case exit(Int32)
+        case uncaughtSignal(Int32)
+    }
+
+    public struct PersonaOptions: Codable {
+        let uid: uid_t
+        let gid: gid_t
+    }
+
+    /// Execution Receipt
+    public struct ExecuteReceipt: Codable {
+        // exit code when process exit,
+        // or signal code when process terminated by signal
+        public let terminationReason: TerminationReason
+        // process pid that was when it is alive
+        // -1 means spawn failed in some situation
+        public let pid: Int
+        // wait result for final waitpid inside block at
+        // processSource - eventMask.exit, usually is pid
+        // -1 for other cases
+        public let wait: Int
+        // any error from us, not the command it self
+        // DOES NOT MEAN THAT THE COMMAND DONE WELL
+        public let error: ExecuteError?
+        // stdout
+        public let stdout: String
+        // stderr
+        public let stderr: String
+
+        /// General initialization of receipt object
+        /// - Parameters:
+        ///   - terminationReason: termination reason
+        ///   - pid: pid when process alive
+        ///   - wait: wait result on waitpid
+        ///   - error: error if any
+        ///   - stdout: stdout
+        ///   - stderr: stderr
+        internal init(
+            terminationReason: TerminationReason,
+            pid: Int,
+            wait: Int,
+            error: AuxiliaryExecute.ExecuteError?,
+            stdout: String,
+            stderr: String
+        ) {
+            self.terminationReason = terminationReason
+            self.pid = pid
+            self.wait = wait
+            self.error = error
+            self.stdout = stdout
+            self.stderr = stderr
+        }
+
+        /// Template for making failure receipt
+        /// - Parameters:
+        ///   - terminationReason: default uncaught signal 0
+        ///   - pid: default -1
+        ///   - wait: default -1
+        ///   - error: error
+        ///   - stdout: default empty
+        ///   - stderr: default empty
+        internal static func failure(
+            terminationReason: TerminationReason = .uncaughtSignal(0),
+            pid: Int = -1,
+            wait: Int = -1,
+            error: AuxiliaryExecute.ExecuteError?,
+            stdout: String = "",
+            stderr: String = ""
+        ) -> ExecuteReceipt {
+            .init(
+                terminationReason: terminationReason,
+                pid: pid,
+                wait: wait,
+                error: error,
+                stdout: stdout,
+                stderr: stderr
+            )
+        }
+    }
+}
--- TrollFools/Execute.swift
+++ TrollFools/Execute.swift
@@ -8,107 +8,22 @@
 import CocoaLumberjackSwift
 import Foundation
 
-@_silgen_name("posix_spawnattr_set_persona_np")
-private func posix_spawnattr_set_persona_np(
-    _ attr: UnsafeMutablePointer<posix_spawnattr_t?>,
-    _ persona_id: uid_t,
-    _ flags: UInt32
-) -> Int32
-
-@_silgen_name("posix_spawnattr_set_persona_uid_np")
-private func posix_spawnattr_set_persona_uid_np(
-    _ attr: UnsafeMutablePointer<posix_spawnattr_t?>,
-    _ persona_id: uid_t
-) -> Int32
-
-@_silgen_name("posix_spawnattr_set_persona_gid_np")
-private func posix_spawnattr_set_persona_gid_np(
-    _ attr: UnsafeMutablePointer<posix_spawnattr_t?>,
-    _ persona_id: uid_t
-) -> Int32
-
-private let POSIX_SPAWN_PERSONA_FLAGS_OVERRIDE = UInt32(1)
-
-private func WIFEXITED(_ status: Int32) -> Bool {
-    _WSTATUS(status) == 0
-}
-
-private func _WSTATUS(_ status: Int32) -> Int32 {
-    status & 0x7f
-}
-
-private func WIFSIGNALED(_ status: Int32) -> Bool {
-    (_WSTATUS(status) != 0) && (_WSTATUS(status) != 0x7f)
-}
-
-private func WEXITSTATUS(_ status: Int32) -> Int32 {
-    (status >> 8) & 0xff
-}
-
-private func WTERMSIG(_ status: Int32) -> Int32 {
-    status & 0x7f
-}
-
 enum Execute {
 
-    enum TerminationReason {
-        case exit(Int32)
-        case uncaughtSignal(Int32)
-    }
-
     @discardableResult
-    static func spawn(
+    static func rootSpawn(
         binary: String,
         arguments: [String] = [],
         environment: [String: String] = [:]
-    ) throws -> TerminationReason {
-
-        var attrs: posix_spawnattr_t?
-        posix_spawnattr_init(&attrs)
-        defer { posix_spawnattr_destroy(&attrs) }
-
-        _ = posix_spawnattr_set_persona_np(&attrs, 99, POSIX_SPAWN_PERSONA_FLAGS_OVERRIDE)
-        _ = posix_spawnattr_set_persona_uid_np(&attrs, 0)
-        _ = posix_spawnattr_set_persona_gid_np(&attrs, 0)
-
-        let env = environment
-            .map { key, value in "\(key)=\(value)" }
-            .reduce(into: [String]()) { $0.append($1) }
-            .map { $0.withCString(strdup) }
-        defer { env.forEach { free($0) } }
-
-        let args = [binary] + arguments
-        let argv: [UnsafeMutablePointer<CChar>?] = args.map { $0.withCString(strdup) }
-        defer { for case let arg? in argv {
-            free(arg)
-        } }
-
-        DDLogInfo("Execute \(binary) \(args.joined(separator: " "))")
-
-        var pid: pid_t = 0
-        let ret = posix_spawn(&pid, binary, nil, &attrs, argv + [nil], env + [nil])
-        if ret != 0 {
-            throw POSIXError(POSIXErrorCode(rawValue: ret)!)
-        }
-
-        var status: Int32 = 0
-        waitpid(pid, &status, 0)
-
-        if WIFSIGNALED(status) {
-            let signal = WTERMSIG(status)
-            DDLogError("Process \(pid) terminated with uncaught signal \(signal)")
-            return .uncaughtSignal(signal)
-        } else {
-            assert(WIFEXITED(status))
-
-            let exitCode = WEXITSTATUS(status)
-            if exitCode == 0 {
-                DDLogInfo("Process \(pid) exited successfully")
-            } else {
-                DDLogError("Process \(pid) exited with code \(exitCode)")
-            }
-
-            return .exit(exitCode)
-        }
+    ) throws -> AuxiliaryExecute.TerminationReason {
+        let receipt = AuxiliaryExecute.spawn(
+            command: binary,
+            args: arguments,
+            environment: environment,
+            personaOptions: .init(uid: 0, gid: 0)
+        )
+        DDLogInfo("Standard output: \(receipt.stdout)")
+        DDLogInfo("Standard error: \(receipt.stderr)")
+        return receipt.terminationReason
     }
 }
