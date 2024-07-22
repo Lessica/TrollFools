86979c792fe767e6b6bbf70853f2625b1f97cb57
82Flex <82flex@gmail.com>
2024-07-22 20:53:04 +0800

    feat: supports ts apps
    Signed-off-by: 82Flex <82flex@gmail.com>

--- TrollFools/Execute.swift
+++ TrollFools/Execute.swift
@@ -30,4 +30,24 @@ enum Execute {
         }
         return receipt.terminationReason
     }
+
+    static func rootSpawnWithOutputs(
+        binary: String,
+        arguments: [String] = [],
+        environment: [String: String] = [:]
+    ) throws -> AuxiliaryExecute.ExecuteReceipt {
+        let receipt = AuxiliaryExecute.spawn(
+            command: binary,
+            args: arguments,
+            environment: environment,
+            personaOptions: .init(uid: 0, gid: 0)
+        )
+        if !receipt.stdout.isEmpty {
+            DDLogInfo("Standard output: \(receipt.stdout)")
+        }
+        if !receipt.stderr.isEmpty {
+            DDLogInfo("Standard error: \(receipt.stderr)")
+        }
+        return receipt
+    }
 }
